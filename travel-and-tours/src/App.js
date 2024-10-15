5e86476a9acf6625f8d830237d1cbe58d4a66706
Habiba Riaz <154079574+habibariaz@users.noreply.github.com>
2024-10-15 17:49:13 +0500

    Add files via upload

---
+++ travel-and-tours/src/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import Hotels from "./Components/Hotels";
+import AboutUs from "./Components/AboutUs";
+import Gallery from "./Components/Gallery";
+import News from "./Components/News";
+import Contact from "./Components/Contact";
+
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index path="/" element={<Home />} />
+          <Route exact path="/Hotels" element={<Hotels />} />
+          <Route exact path="/AboutUs" element={<AboutUs />} />
+          <Route exact path="/Gallery" element={<Gallery />} />
+          <Route exact path="/News" element={<News />} />
+          <Route exact path="/Contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+
+    </>
+  );
+}
+
+export default App;
